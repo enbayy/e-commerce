f06ae10f74743449fbf98f6de52a15d70e400f09
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2024-12-23 02:49:49 +0300

    contactForm added

--- src/pages/contactPage/page.jsx
+++ src/pages/contactPage/page.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 
 const ContactPage = () => {
     return (
-        <div>ContactPage</div>
+        <div>
+            <div className="w-full h-screen flex justify-center items-center bg-gray-100">
+                <form className="bg-white shadow-lg rounded-lg p-8 w-1/3">
+                    <h2 className="text-2xl font-bold text-center mb-6 text-gray-700">İletişim Formu</h2>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
+                            İsim
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="Adınızı girin"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+                            E-posta Adresi
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="E-posta adresinizi girin"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="message" className="block text-gray-700 font-medium mb-2">
+                            Mesaj
+                        </label>
+                        <textarea
+                            id="message"
+                            rows="4"
+                            placeholder="Mesajınızı buraya yazın"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        ></textarea>
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-gray-600 text-white py-2 rounded-lg hover:bg-black transition duration-200"
+                    >
+                        Gönder
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
